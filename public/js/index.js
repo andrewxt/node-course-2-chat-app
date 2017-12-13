8f01032adff80ce84e14339db431c08f4aa75772
Drew Christie <achristie@vandis.com>
2017-12-13 12:07:15 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 socket.on('connect', function () {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    to: 'hanna',
-    text: 'hey love'
-  });
 });
 
 socket.on('newMessage', function (message) {
